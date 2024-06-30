77dbb6818f2174e76e0d56481de9f7385b5d8a90
shudh <shudh@DESKTOP-G0DBO7M>
2024-06-30 21:13:16 +0530

    Add project files.

---
+++ AuthenticationApp/Controllers/DashboardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Dotnet6MvcLogin.Models.Domain;
+using System.Threading.Tasks;
+
+namespace Dotnet6MvcLogin.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DashboardController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Display()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                ViewBag.FirstName = user.FirstName;
+                ViewBag.LastName = user.LastName;
+            }
+
+            return View();
+        }
+    }
+}
